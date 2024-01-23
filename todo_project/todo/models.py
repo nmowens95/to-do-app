e08e42a1172fd2ec7daaf90e977566f250c60890
Nathan Owens <nmowens95@gmail.com>
2024-01-23 17:10:25 -0600

    Set up first basic model for a task

---
+++ todo_project/todo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Todo(models.Model):
+    name = models.CharField(max_length=80)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_completed = models.BooleanField(default=False)
