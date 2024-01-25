aa5bb00f9b483562502c321907f2a01e1b341ef6
Nathan Owens <nmowens95@gmail.com>
2024-01-24 22:18:10 -0600

    Wrote out basic crud operations for a todo app

--- todo_project/api/urls.py
+++ todo_project/api/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.getTask),
+     path('tasks', views.get_tasks, name='get_tasks'),
+     path('tasks/<int:pk>/', views.get_task, name='get_task'),
+     path('tasks/new/', views.post_task, name='post_task'),
+     path('tasks/<int:pk>/update/', views.update_task, name='update_task'),
+     path('tasks/<int:pk>/delete/', views.delete_task, name='delete_task'),
 ]
