8d83aea367bf762241a17f70d7fcd8fdb1f4b0f3
Nathan Owens <nmowens95@gmail.com>
2024-01-25 16:26:32 -0600

    Set up so that there was context for the html im rendering

--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Todo
 
 # Create your views here.
 def todo(request):
+    todos = Todo.objects.all()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    context = {'todos': todos}
+    return HttpResponse(template.render(context, request))
