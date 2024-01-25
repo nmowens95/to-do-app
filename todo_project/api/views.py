e945a925eca07c85bb4c230bd2dd62e07a37e4e6
Nathan Owens <nmowens95@gmail.com>
2024-01-24 22:19:17 -0600

    Wrote out logic for crud operations, need to look into some of the django shortcuts, but got to learn about implementing serialization and some syntax

--- todo_project/api/views.py
+++ todo_project/api/views.py
@@ -1,26 +1,68 @@
 # This will serialize and render out our data to json automatically
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 from todo.models import Todo
 from .serializers import ItemSerializer
+# shortcut for error handling
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 @api_view(['GET'])
-def getTask(request):
+def get_tasks(request):
+    """
+    Get all tasks
+    """
     tasks = Todo.objects.all()
     serializer = ItemSerializer(tasks, many=True)
     return Response(serializer.data)
     
+@api_view(['GET'])
+def get_task(request, pk):
+    """
+    Get a singular task with a given ID
+    """
+    task = get_object_or_404(Todo, pk=pk)
+    serializer = ItemSerializer(task)
+    return Response(serializer.data)
 
 @api_view(['POST'])
-def postData(request):
-    pass
+def post_task(request):
+    """
+    Create a new task
+    """
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['PUT'])
-def putData(request):
-    pass
+def update_task(request, pk):
+    """
+    Update a task with the given id
+    """
+    try: 
+        task = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response({'Error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['Delete'])
-def deleteData(request):
-    pass
+    serializer = ItemSerializer(task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_task(request, pk):
+    """
+    Delete a task with a given ID
+    """
+    try:
+        task = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response({'Error:':'Task not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    task.delete()
+    return Response({'Message': 'Task deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
