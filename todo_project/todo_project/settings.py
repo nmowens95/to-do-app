5d3f8649d57b9dbe4481faa5d9965bd232734a15
Nathan Owens <nmowens95@gmail.com>
2024-01-27 17:04:24 -0600

    learned later about the secret key and added it to my .env file. I also added the auth user model for our todo.User

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -25,12 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^5fs09%2pl529^&8w^*qc82kypp!)r+qit8x4bopd89%-v3d^%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+AUTH_USER_MODEL =  "todo.User"
 
 
 # Application definition
