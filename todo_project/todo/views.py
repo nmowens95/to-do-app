43542ec867deec6497e695dee55c38953ee4a342
Nathan Owens <nmowens95@gmail.com>
2024-01-29 21:47:28 -0600

    Added the signup.html path and render. Not totally functional yet but will need to apply adjustments

--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 from .models import Todo
 
 # Create your views here.
@@ -8,3 +10,9 @@ def todo(request):
     template = loader.get_template('index.html')
     context = {'todos': todos}
     return HttpResponse(template.render(context, request))
+
+def signup(request):
+    form = UserCreationForm()
+    return render(request, 'signup.html', {
+        'form': form
+    })
